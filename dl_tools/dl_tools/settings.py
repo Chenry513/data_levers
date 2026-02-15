c4b0d70572c5efbc8b2eeeec97ddcbd540053bec
Henry Chen <99457027+Chenry513@users.noreply.github.com>
2026-02-14 18:15:26 -0800

    Update settings.py

--- dl_tools/dl_tools/settings.py
+++ dl_tools/dl_tools/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-_srnngabtd=ln-36oruw$ha!rgrl0#j^#4$hx(9k=49*@qr9-v')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
@@ -97,7 +97,7 @@ ROOT_URLCONF = 'dl_tools.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'frontend'],
+        'DIRS': [BASE_DIR / 'frontend' / 'dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
